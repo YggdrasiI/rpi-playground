67e0894b18f09dabb670c75e09fe0906a2727fd9
Olaf Schulz <funwithkinect@googlemail.com>
2016-07-27 11:38:10 +0200

    Add example with branch instruction examles.

---
+++ QPU/test_branching/branching.asm
@@ -0,0 +1,151 @@
+## Test different cases of branching.
+# If a branch is taken, the branch id will be stored
+# (as element) to ra_taken_register.
+# Finally this register will returned to arm side.
+
+#############################################################
+.set rb_first_uniform,         rb3
+.set ra_taken_register,        ra2
+#############################################################
+## Change register at singe position.
+#  reg[index] = value
+# Third operation added to fill all three branch delay slots.
+.macro change_elem, reg, index, value
+  sub.setf r0, elem_num, index
+  mov.ifz reg, value
+  nop
+.endm
+#############################################################
+
+mov ra_taken_register, 0
+
+# Get the input value (first uniform, internally loaded over rb32)
+# Currently, value not used.
+mov rb_first_uniform, unif
+
+#############################################################
+## Brancing
+
+## 1. Relative branch without care of destination address.
+# Could be read as IF(NOT cond){
+# Lines between brr and jump target.
+# [...]
+# }JUMP_TARGET
+#
+# Remember that branch instructions are executed 3 instructions delayed,
+# i.e. three further instructions are always executed.
+# 
+change_elem ra_taken_register, 0, 1
+mov.setf r0, elem_num        # 0, 1, 2, …
+brr.anyz -, r:1f 
+nop # Three branch
+nop # delay slots.
+nop # Do not use second branch here.
+
+    # Next line only reached if branch not taken
+    change_elem ra_taken_register, 0, 0
+
+# Target of branch
+:1
+nop
+
+
+## 2. Similar to example 1, but more compact 
+sub.setf -, 0, elem_num  # 0, -1, -2, …
+brr.anynn -, r:1f        # Any not negative
+# Use macro to fill
+# delay slots.
+change_elem ra_taken_register, 1, 2
+
+    # Next line only reached if branch not taken
+    change_elem ra_taken_register, 1, 0
+
+# Same label as above. The 'f' flag at the branch let select this one.
+# This is useful if a multiple used macro defines a label.
+:1
+
+
+## 3. Use destination register to store jump address for later usage.
+# Take care not to trap yourself into an infinite loop...
+#
+.set ra_address,     ra10
+
+# Store program counter for a label
+# As there is no condition the branch is always taken. Thus,
+# the fourth instruction after this branch will never reached 'linearly'.
+brr ra_address,      r:loop_head
+
+# Store number of repeats for the following while loop.
+mov r0, 3                # Loop counter
+nop
+nop
+
+:loop_head
+
+# Dummy operations. Use second byte in ra10[2] to count the loops.
+sub.setf r1, elem_num, 2    # Zero on third position
+add.ifz ra_taken_register, ra_taken_register, 1
+nop
+nop
+
+sub.setf r0, r0, 1
+# Jump to head of loop if r0>0 (branch absolute!)
+bra.allnz -, ra_address
+nop
+nop
+nop
+
+# Print out address
+#sub.setf r1, elem_num, 15
+#mov r0, ra_address
+#add.ifz ra_taken_register, ra_taken_register, r0
+
+## 4. If-Then-Else
+# This example uses three branch instrutions. Swap
+# then and else branch to reduce it on two.
+
+shl.setf r0, elem_num, 4
+#mov.setf r0, -1
+
+brr.allnn -, r:then
+nop
+nop # Two nop's seems enough.
+brr.anyn -, r:else 
+nop
+nop
+
+:then
+change_elem ra_taken_register, 3, 4
+nop         # Omit overlapping warning of .back
+
+# Shift branch up to use delay slots.
+.back 3
+brr -, r:end
+.endb
+
+:else
+change_elem ra_taken_register, 3, -4
+
+:end
+
+#############################################################
+
+# Configure the VPM for writing
+# See vc4asm documentation and test_vpm_write for more details.
+mov vw_setup, vpm_setup(1, 16, v32(0, 0))
+
+# Write into the VPM.
+mov vpm, ra_taken_register
+
+## move 16 words (1 vector) back to the host (DMA)
+mov vw_setup, vdw_setup_0(1, 16, dma_v32(0, 0))
+mov vw_setup, vdw_setup_1(0) # stride
+
+## initiate the DMA (the next uniform - ra32 - is the host address to write to))
+mov vw_addr, unif
+
+# Wait for the DMA to complete
+read vw_wait
+
+# Trigger a host interrupt (writing rb38) to stop the program
+exit 0
---
+++ QPU/test_branching/driver.c
@@ -0,0 +1,130 @@
+/* Test code to understand the difficulties of
+ * the brr and bra commands and
+ * how to use flags with .setf
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <sys/time.h>
+
+#include "mailbox.h"
+#include "qpu.h"
+
+#define NUM_QPUS        1
+#define MAX_CODE_SIZE   8192
+
+static unsigned int qpu_code[MAX_CODE_SIZE];
+
+struct memory_map {
+    unsigned int code[MAX_CODE_SIZE];
+    unsigned int uniforms[NUM_QPUS][2];     // 2 parameters per QPU
+                                            // first address is the input value
+                                            // for the program to add to
+                                            // second is the address of the
+                                            // result buffer
+    unsigned int msg[NUM_QPUS][2];
+    unsigned int results[NUM_QPUS][16];     // result buffer for the QPU to
+                                            // write into
+};
+
+
+int loadShaderCode(const char *fname, unsigned int* buffer, int len)
+{
+    FILE *in = fopen(fname, "r");
+    if (!in) {
+        fprintf(stderr, "Failed to open %s.\n", fname);
+        exit(0);
+    }
+
+    size_t items = fread(buffer, sizeof(unsigned int), len, in);
+    fclose(in);
+
+    return items;
+}
+
+/* Uniform value for qpu 
+ * Note: Sometimes, I've got the wrong
+ * uniform on QPU X (it uses the value from X+1) in some of the four
+ * multiplexed runs.
+ */
+unsigned int uniform_map(int i ){
+    return (unsigned int) i;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <code .bin> <val>\n", argv[0]);
+        return 0;
+    }
+    int code_words = loadShaderCode(argv[1], qpu_code, MAX_CODE_SIZE);
+
+    printf("Loaded %d bytes of code from %s ...\n", code_words * sizeof(unsigned), argv[1]);
+
+    int mb = mbox_open();
+
+    struct GPU_FFT_HOST host;
+    if (gpu_fft_get_host_info(&host)){
+        fprintf(stderr, "QPU fetch of host information (Rpi version, etc.) failed.\n");
+        return -5;
+    }
+
+    if (qpu_enable(mb, 1)) {
+        fprintf(stderr, "QPU enable failed.\n");
+        return -1;
+    }
+    printf("QPU enabled.\n");
+
+    unsigned uniform_val = atoi(argv[2]); // unused
+    //printf("Uniform value = %d\n", uniform_val);
+
+    unsigned size = 1024 * 1024;
+    unsigned handle = mem_alloc(mb, size, 4096, host.mem_flg);
+    if (!handle) {
+        fprintf(stderr, "Unable to allocate %d bytes of GPU memory", size);
+        return -2;
+    }
+
+    volatile unsigned *peri = (volatile unsigned *) mapmem(host.peri_addr, host.peri_size);
+    if (!peri) {
+        mem_free(mb, handle);
+        qpu_enable(mb, 0);
+        return -4;
+    }
+
+    unsigned ptr = mem_lock(mb, handle);
+    void *arm_ptr = mapmem(BUS_TO_PHYS(ptr + host.mem_map), size);
+    // assert arm_ptr ...
+
+    struct memory_map *arm_map = (struct memory_map *)arm_ptr;
+    memset(arm_map, 0x0, sizeof(struct memory_map));
+    unsigned vc_uniforms = ptr + offsetof(struct memory_map, uniforms);
+    unsigned vc_code = ptr + offsetof(struct memory_map, code);
+    unsigned vc_msg = ptr + offsetof(struct memory_map, msg);
+    unsigned vc_results = ptr + offsetof(struct memory_map, results);
+    memcpy(arm_map->code, qpu_code, code_words * sizeof(unsigned int));
+    for (int i=0; i < NUM_QPUS; i++) {
+        arm_map->uniforms[i][0] = uniform_map(i);//uniform_val;
+        arm_map->uniforms[i][1] = vc_results + i * sizeof(unsigned) * 16;
+        arm_map->msg[i][0] = vc_uniforms + i * sizeof(unsigned) * 2;
+        arm_map->msg[i][1] = vc_code;
+    }
+
+    unsigned ret = execute_qpu(mb, NUM_QPUS, vc_msg, GPU_FFT_NO_FLUSH, GPU_FFT_TIMEOUT);
+
+    // check the results!
+    printf("\nBranch not taken: id = 0\n    Branch taken: id > 0\n");
+    for (int j=0; j < 16; j++) {
+        printf("Word %2d:\t\t%4u\n",
+                j+1, arm_map->results[0][j]);
+    }
+
+    printf("Cleaning up.\n");
+    unmapmem(arm_ptr, size);
+    unmapmem((void*)host.peri_addr, host.peri_size);
+    mem_unlock(mb, handle);
+    mem_free(mb, handle);
+    qpu_enable(mb, 0);
+    printf("Done.\n");
+}
